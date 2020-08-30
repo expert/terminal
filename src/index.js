e883943077697da26cbce9676f14475c2050de44
alexei <fktjif@gmail.com>
2020-08-30 12:47:41 +0300

    Terminal design

--- src/index.js
+++ src/index.js
@@ -1,17 +1,50 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import Tab from "./Tab";
+
+class Console extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            date: new Date(),
+            counter: 1,
+            isLoggedIn: true
+        };
+
+    }
+    componentDidMount() {
+        // this.timerID = setInterval(
+        //     () => this.tick(),
+        //     1000
+        // );
+    }
+
+    componentWillUnmount() {
+        // clearInterval(this.timerID);
+    }
+
+    // tick() {
+    //     this.setState({
+    //         date: new Date()
+    //     });
+    // }
+    render() {
+
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <Tab />
+            </div>
+
+        );
+    }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <Console />,
+    document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
